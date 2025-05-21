ca8f8b023f23ba14ba19b231789fed1b2afe86ce
abdullahyazgi <abdullah@yazgi.se>
2025-05-21 11:39:41 +0300

    Select2

--- src/components/MuiSelect.tsx
+++ src/components/MuiSelect.tsx
@@ -1,52 +1,76 @@
-import { Box, TextField, MenuItem, Stack } from "@mui/material";
+// import { Box, TextField, MenuItem, Stack } from "@mui/material";
+import {
+  InputLabel,
+  FormControl,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+} from "@mui/material";
 import { useState } from "react";
 
 const MuiSelect = () => {
   const [country, setCountry] = useState("");
   console.log({ country });
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setCountry(event.target.value as string);
+  const handleChange = (event: SelectChangeEvent) => {
+    setCountry(event.target.value);
   };
 
-  const [languages, setLanguages] = useState<string[]>([]);
-  console.log({ languages });
-  const handlelanguages = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.value;
-    setLanguages(typeof value === "string" ? value.split(",") : value);
-  };
+  // const [languages, setLanguages] = useState<string[]>([]);
+  // console.log({ languages });
+  // const handlelanguages = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   const value = event.target.value;
+  //   setLanguages(typeof value === "string" ? value.split(",") : value);
+  // };
 
   return (
-    <Stack spacing={2}>
-      <Box width="250px">
-        <TextField
-          label="Select Country"
-          select
+    <div>
+      <FormControl sx={{ m: 1, minWidth: 250 }}>
+        <InputLabel id="select-country">Select Country</InputLabel>
+        <Select
+          labelId="select-country"
+          id="selected-counrty"
           value={country}
+          label="Select Country"
           onChange={handleChange}
-          fullWidth
         >
           <MenuItem value="SY">Syria</MenuItem>
           <MenuItem value="USA">USA</MenuItem>
           <MenuItem value="AU">Australia</MenuItem>
-        </TextField>
-      </Box>
-      <Box width="250px">
-        <TextField
-          label="Select Languags Programing"
-          select
-          value={languages}
-          onChange={handlelanguages}
-          fullWidth
-          SelectProps={{
-            multiple: true,
-          }}
-        >
-          <MenuItem value="JA">Java</MenuItem>
-          <MenuItem value="JS">JavaScript</MenuItem>
-          <MenuItem value="PY">Python</MenuItem>
-        </TextField>
-      </Box>
-    </Stack>
+        </Select>
+      </FormControl>
+    </div>
+
+    // <Stack spacing={2}>
+    //   <Box width="250px">
+    //     <TextField
+    //       label="Select Country"
+    //       select
+    //       value={country}
+    //       onChange={handleChange}
+    //       fullWidth
+    //     >
+    //       <MenuItem value="SY">Syria</MenuItem>
+    //       <MenuItem value="USA">USA</MenuItem>
+    //       <MenuItem value="AU">Australia</MenuItem>
+    //     </TextField>
+    //   </Box>
+    //   <Box width="250px">
+    //     <TextField
+    //       label="Select Languags Programing"
+    //       select
+    //       value={languages}
+    //       onChange={handlelanguages}
+    //       fullWidth
+    //       SelectProps={{
+    //         multiple: true,
+    //       }}
+    //     >
+    //       <MenuItem value="JA">Java</MenuItem>
+    //       <MenuItem value="JS">JavaScript</MenuItem>
+    //       <MenuItem value="PY">Python</MenuItem>
+    //     </TextField>
+    //   </Box>
+    // </Stack>
   );
 };
 
