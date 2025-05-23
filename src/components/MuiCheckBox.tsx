d959b864f95aa6310007aec20d4746cec8b01c86
abdullahyazgi <abdullah@yazgi.se>
2025-05-23 10:59:44 +0300

    MuiCheckBox

---
+++ src/components/MuiCheckBox.tsx
@@ -0,0 +1,95 @@
+import {
+  Box,
+  Checkbox,
+  FormControlLabel,
+  FormControl,
+  FormLabel,
+  FormGroup,
+  FormHelperText,
+} from "@mui/material";
+import { useState } from "react";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
+
+const MuiCheckBox = () => {
+  const [accept, setAccept] = useState(false);
+  const [skills, setSkills] = useState<string[]>([]);
+  // console.log({ accept });
+  console.log({ skills });
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAccept(event.target.checked);
+  };
+  const handleSkillChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const index = skills.indexOf(event.target.value);
+    if (index === -1) {
+      setSkills([...skills, event.target.value]);
+    } else {
+      setSkills(skills.filter((skill) => skill !== event.target.value));
+    }
+  };
+  return (
+    <Box>
+      <Box>
+        <FormControlLabel
+          label="I'am Learning MUI"
+          control={
+            <Checkbox
+              size="small"
+              color="secondary"
+              checked={accept}
+              onChange={handleChange}
+            />
+          }
+        />
+      </Box>
+      <Box>
+        <Checkbox
+          icon={<BookmarkBorderIcon />}
+          checkedIcon={<BookmarkIcon />}
+          checked={accept}
+          onChange={handleChange}
+        />
+      </Box>
+      <Box>
+        <FormControl error>
+          <FormLabel>Skills</FormLabel>
+          <FormGroup row>
+            <FormControlLabel
+              label="HTML"
+              control={
+                <Checkbox
+                  value="html"
+                  checked={skills.includes("html")}
+                  onChange={handleSkillChange}
+                />
+              }
+            />
+            <FormControlLabel
+              label="CSS"
+              value="CSS"
+              control={
+                <Checkbox
+                  checked={skills.includes("css")}
+                  onChange={handleSkillChange}
+                />
+              }
+            />
+            <FormControlLabel
+              label="JavaScript"
+              value="JavaScript"
+              control={
+                <Checkbox
+                  checked={skills.includes("javascript")}
+                  onChange={handleSkillChange}
+                />
+              }
+            />
+          </FormGroup>
+          <FormHelperText>Invalid selection</FormHelperText>
+        </FormControl>
+      </Box>
+    </Box>
+  );
+};
+
+export default MuiCheckBox;
